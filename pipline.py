48b18a313c95156b75e3028e3d13f46f02f65766
whoami-j <whoami-j>
2024-10-30 04:22:11 +0000

    modified pipeline & Test and add conf

--- pipline.py
+++ pipline.py
@@ -1,11 +1,19 @@
 # Databricks notebook source
 import pandas as pd
 import math
+import os
+import configparser
 
-# file path
+# Get the parent path of the notebook
+notebook_path = dbutils.entry_point.getDbutils().notebook().getContext().notebookPath().get()
+parent_path = os.path.dirname('/Workspace' + notebook_path)
+os.chdir(parent_path)
 
-inputPath = "/Workspace/Users/jakkitli@ais.co.th/track_small.csv"
-outputPath = "/Workspace/Users/jakkitli@ais.co.th/output_small.csv"
+# read config file
+config = configparser.ConfigParser()
+config.read('./pipeline.conf')
+inputPath = config.get('DEFAULT', 'INPUT_PATH')
+outputPath = config.get('DEFAULT', 'OUTPUT_PATH')
 
 # Extract
 tracks = pd.read_csv(inputPath)
@@ -13,13 +21,9 @@ tracks = pd.read_csv(inputPath)
 # Transform
 tracks["UnitPrice"] = tracks["UnitPrice"].apply(lambda x: math.ceil(x))
                              
-# # Load
+# Load
 tracks.to_csv(outputPath, index=False)
 
 # COMMAND ----------
 
 tracks.head()
-
-# COMMAND ----------
-
-
